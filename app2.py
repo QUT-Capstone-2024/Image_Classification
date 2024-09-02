cfb5f2ad8d6e104822f57300d14e00b25096be69
akknr3 <hamza.hirai@connect.qut.edu.au>
2024-09-03 09:53:00 +1000

    API stuoff

--- app2.py
+++ app2.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 from flask import Flask, request, jsonify
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 import io
+import mysql.connector
 import boto3
-from tensorflow.keras.models import load_model
 
 app = Flask(__name__)
 
@@ -13,6 +19,14 @@ model = tf.keras.models.load_model('mobilenetv2_house_rooms_model.h5')
 
 # Define the categories
 categories = ['Bathroom', 'Bedroom', 'Dinning', 'Kitchen', 'Livingroom', 'Street']
+
+# Database connection setup
+db = mysql.connector.connect(
+    host="localhost",
+    user="centralapi_user",
+    password="password",
+    database="central_api"
+)
 
 # S3 Client setup
 s3_client = boto3.client(
@@ -23,39 +37,43 @@ s3_client = boto3.client(
 )
 
 def preprocess_image(image_bytes):
-    IMAGE_SIZE = (128, 128)
-    image = tf.image.decode_jpeg(image_bytes, channels=3)
-    image = tf.image.central_crop(image, central_fraction=0.8)
-    image = tf.image.resize(image, IMAGE_SIZE)
-    image = tf.image.convert_image_dtype(image, tf.float32)
-    image = tf.expand_dims(image, axis=0)
+    image = Image.open(io.BytesIO(image_bytes))
+    image = image.resize((128, 128))
+    image = np.array(image) / 255.0
+    image = np.expand_dims(image, axis=0) 
     return image
 
 @app.route('/api/image/classify', methods=['POST'])
 def classify_image():
-    data = request.get_json()
-    
-    if 'url' not in data:
-        return jsonify({'error': 'No URL provided'}), 400
+    if 'image_id' not in request.form:
+        return jsonify({'error': 'No image ID provided'}), 400
 
-    image_url = data['url']
+    image_id = request.form['image_id']
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT url FROM image WHERE id = %s", (image_id,))
+    image_record = cursor.fetchone()
+    cursor.close()
+
+    if not image_record:
+        return jsonify({'error': 'Image not found'}), 404
+    image_url = image_record['url']
     bucket_name = 'visioncore-image-bucket' 
     key = image_url.split(f'https://{bucket_name}.s3.ap-southeast-2.amazonaws.com/')[1]
-
     try:
         image_object = s3_client.get_object(Bucket=bucket_name, Key=key)
         image_bytes = image_object['Body'].read()
     except Exception as e:
         return jsonify({'error': 'Error retrieving image from S3', 'details': str(e)}), 500
-
     image = preprocess_image(image_bytes)
     predictions = model.predict(image)[0]
     confidence_scores = {category: float(score) for category, score in zip(categories, predictions)}
 
     return jsonify({
+        'image_id': image_id,
         'confidence_scores': confidence_scores,
-        'file_name': image_url
+        'file_name' : image_url
     })
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
